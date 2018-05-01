f867bf1facbf2ec11cf3ac081e7ce8128f6cac11
vinicius <dutrad.vinicius@gmail.com>
2018-05-01 16:08:54 -0300

    ARMA

--- R/ARMA/arma_stock.R
+++ R/ARMA/arma_stock.R
@@ -1,14 +1,13 @@
-#if you do not have already installed you have to do so
-install.packages("quantmod")
-
 #this is how we can fetch finance related date from the web
 require(quantmod)
 
 #we download S&P500 prices from Yahoo Finance
 getSymbols("^GSPC",src="yahoo")
 
+PETR <- read.csv(file="..\\Data\\SP500.csv", header=TRUE, sep=",")
+
 #log daily returns
-returns <- diff(log(Ad(GSPC)))
+returns <- diff(log(PETR$Adj.Close))
 
 #calculate the optimal q and p values with AIC
 solution.aic <- Inf
@@ -18,10 +17,10 @@ solution.order <- c(0,0,0)
 #and choose the best according to the AIC
 for(i in 1:4) for(j in 1:4) { 
   
-  actual.aic <- AIC(arima(returns,order=c(i,0,j),optim.control=list(maxit = 1000)))
+  actual <- arima(returns,order=c(i,0,j),optim.control=list(maxit = 1000))
   
-  if( actual.aic < solution.aic){
-    solution.aic <- actual.aic
+  if( actual$aic < solution.aic){
+    solution.aic <- actual$aic
     solution.order <- c(i,0,j)
     solution.arma <- arima(returns,order=solution.order,optim.control=list(maxit = 1000))
   }
@@ -31,7 +30,7 @@ for(i in 1:4) for(j in 1:4) {
 solution.order
 
 #autocorrelation plot
-acf(resid(solution.arma),na.action=na.omit)
+acf(resid(solution.arma),na.action=na.omit, lag.max = 35)
 
 #Ljung-Box test for the y(t) residual series
 Box.test(resid(solution.arma),lag=20,type="Ljung-Box")
--- R/MA/ma_stock.R
+++ R/MA/ma_stock.R
@@ -3,18 +3,20 @@ require(quantmod)
 
 PETR <- read.csv(file="..\\Data\\SP500.csv", header=TRUE, sep=",")
 
+getSymbols("AAPL", src= "yahoo")
+
 adClose <- PETR$Adj.Close
 #we want to plot the adjusted closing prices
-plot(adClose)
+plot(Ad(AAPL))
 
 #log daily returns of AAPL stock (for adjusted closing price)
-aaplreturn = diff(log(adClose))
+aaplreturn = diff(log(Ad(AAPL)))
 
 plot(aaplreturn)
 acf(aaplreturn,na.action=na.omit)
 
 #we set AR=0 and integrated component=0 so it is a MA(3)
-aaplreturn.ma <- arima(aaplreturn,order=c(0,0,2))
+aaplreturn.ma <- arima(aaplreturn,order=c(0,0,3))
 aaplreturn.ma
 
 #drop the first value because it is NA
